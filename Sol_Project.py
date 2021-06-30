390942b66139ddc5a7e50e57e3cdf85101ac738e
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-30 18:32:01 +0530

    Add files via upload

---
+++ Sol_Project.py
@@ -0,0 +1,8 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("Excellent Work!")
+elif marks >= 60:
+    print("Good Work!")
+elif marks < 60:
+    print("You can do better!")
---
+++ Sol_TA_1.py
@@ -0,0 +1,11 @@
+age = 30
+if age < 0:
+    print('Not a valid age')
+elif 0 < age < 18:
+    print('You are a teenager')
+elif age == 18:
+    print('You have just entered adulthood')
+elif 19 < age < 21:
+    print('You are about to complete your graduation.')
+else:
+    print('You are in the economical group')
